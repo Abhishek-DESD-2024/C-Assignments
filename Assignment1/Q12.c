d73c7ea8d562136f5ae95108971556f34c3b4787
Abhi-Bachiphale <abhishekbachiphale@gmail.com>
2024-03-15 17:45:31 +0530

    Q12 Assign. 1

---
+++ Assignment1/Q12.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+
+int main()
+{
+
+	printf("--------------------------------------------------------------------------------------------------------------\n");
+	printf("Data Type             Size                      FS                                    Range                    \n");
+    printf("--------------------------------------------------------------------------------------------------------------\n");
+	printf("char                  %ld                      %%c                                  %d  to  %d    \n",sizeof(char),SCHAR_MIN,SCHAR_MAX );
+	printf("unsigned char         %ld                      %%c                                  %d  to  %d    \n",sizeof(char),0,UCHAR_MAX );
+	printf("short int             %ld                      %%hd                                 %d  to  %d    \n",sizeof(short int),SHRT_MIN,SHRT_MAX );
+	printf("unsigned short int    %ld                      %%hu                                 %d  to  %d    \n",sizeof(unsigned short int),0,USHRT_MAX );
+	printf("int                   %ld                      %%d                                  %d  to  %d    \n",sizeof(int),INT_MIN,INT_MAX );
+	printf("unsigned int          %ld                      %%u                                  %d  to  %d    \n",sizeof(unsigned int),0,UINT_MAX );
+	printf("long int              %ld                      %%ld                                 %ld to  %ld    \n",sizeof(long int),LONG_MIN,LONG_MAX );
+	printf("unsigned long int     %ld                      %%lu                                 %d  to  %lu    \n",sizeof(unsigned long int),0,ULONG_MAX );
+	printf("float                 %ld                      %%f                                 %.10e to  %.10e    \n",sizeof(float),FLT_MIN,FLT_MAX );
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
